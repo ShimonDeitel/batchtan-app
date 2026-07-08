3e010c2ef39a50bbbea6e5e834653662a60d1fa1
rex-del <150361329+rex-del@users.noreply.github.com>
2026-07-08 22:27:25 +0300

    Fix duplicate property/init param in model struct

--- iOS/BatchTan/Models.swift
+++ iOS/BatchTan/Models.swift
@@ -8,14 +8,7 @@ struct Hide: Identifiable, Codable, Equatable {
     var notes: String
     var createdDate: Date
 
-    init(
-        id: UUID = UUID(),
-        species: String = "Whitetail Deer",
-        stage: String = "Fleshing",
-        soakStart: Date = Date(),
-        notes: String = "",
-        createdDate: Date = Date()
-    ) {
+    init(id: UUID = UUID(), species: String = "Whitetail Deer", stage: String = "Fleshing", soakStart: Date = Date(), notes: String = "", createdDate: Date = Date()) {
         self.id = id
         self.species = species
         self.stage = stage
@@ -34,14 +27,7 @@ struct BTProEntry: Identifiable, Codable, Equatable {
     var durationHours: String
     var createdDate: Date
 
-    init(
-        id: UUID = UUID(),
-        chemical: String = "Pickle Acid",
-        ratio: String = "1oz per gal @ pH2",
-        stage: String = "Pickling",
-        durationHours: String = "48",
-        createdDate: Date = Date()
-    ) {
+    init(id: UUID = UUID(), chemical: String = "Pickle Acid", ratio: String = "1oz per gal @ pH2", stage: String = "Pickling", durationHours: String = "48", createdDate: Date = Date()) {
         self.id = id
         self.chemical = chemical
         self.ratio = ratio
